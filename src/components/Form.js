8af10a37d7e0f60e4c9b82b8f616cdd1b24bcca0
alvaroazambuja <alvaromello61@gmail.com>
2022-07-12 08:24:22 -0400

    usestate

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,43 @@
+import { useState } from "react";
 
 function Form() {
-    function cadastrarUsuario(e) {
-            e.preventDefault()
-            console.log('cadastrou usuario')
-    }
-    return(
+  function cadastrarUsuario(e) {
+    e.preventDefault();
+    console.log(name);
+    console.log(password);
+    console.log("cadastrou usuario");
+  }
+  const [name, setName] = useState();
+  const [password, setpassword] = useState();
+  return (
+    <div>
+      <h1>Meucadstro com git hub</h1>
+      <form onSubmit={cadastrarUsuario}>
         <div>
-            <h1>Meucadstro com git hub</h1>
-            <form onSubmit={cadastrarUsuario} >
-                <div>
-                    <input type="text" name="" id="" placeholder="Digite seu nome"/>
-                </div>
-                <div>
-                    <input type="submit" value='cadastrar' />
-                </div>
-            </form>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Digite seu nome"            
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+            onChange={(e) => setpassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="cadastrar" />
+        </div>
+      </form>
+    </div>
+  );
 }
-export default Form
+export default Form;
